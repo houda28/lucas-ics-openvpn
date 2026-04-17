3891fc3638fa6dfb7e102f7c9b52555688690c3c
Lucas shu <hshu@sonicwall.com>
2026-04-17 17:01:08 +0800

    analyse the openvpn design

--- main/src/ui/java/de/blinkt/openvpn/activities/MainActivity.kt
+++ main/src/ui/java/de/blinkt/openvpn/activities/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.os.trace
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
